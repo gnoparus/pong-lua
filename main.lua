d50de70965067f0f07749400824ff59b00670867
gnoparus <surapong@gmail.com>
2022-10-26 20:15:52 +0700

    Bounce player and wall

--- main.lua
+++ main.lua
@@ -18,6 +18,7 @@ PADDLE_SPEED = 200
 function love.load()
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle("Pong Lua")
 
     math.randomseed(os.time())
 
@@ -44,6 +45,43 @@ function love.load()
 end
 
 function love.update(dt)
+
+    if gameState == 'play' then
+        -- bounce player1
+        if ball:collides(player1) then
+            ball.x = player1.x + 5
+            ball.dx = -ball.dx * 1.05
+
+            if ball.dy > 0 then
+                ball.dy = math.random(10, 150)
+            else
+                ball.dy = -math.random(10, 150)
+            end
+        end
+
+        -- bounce player2
+        if ball:collides(player2) then
+            ball.x = player2.x - 4
+            ball.dx = -ball.dx * 1.05
+
+            if ball.dy > 0 then
+                ball.dy = math.random(10, 150)
+            else
+                ball.dy = -math.random(10, 150)
+            end
+        end
+
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.dy = -ball.dy
+        end
+
+        if ball.y >= (VIRTUAL_HEIGHT - 4) then
+            ball.y = VIRTUAL_HEIGHT - 4
+            ball.dy = -ball.dy
+        end
+
+    end
 
     -- move player1
     if love.keyboard.isDown('w') then
